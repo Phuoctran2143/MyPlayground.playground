e19ef7ee7a73fa92f8af56877eb70b276e692886
Phuoctran2143 <ptra1705@740s-pl-122.local>
2016-12-12 09:54:03 -0700

    playground

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+for count in -5...10
+{
+    print(count)
+}
+
+for countTwo in 1...maxRanks()
+{
+    print("Phuoc counts to \(countTwo)")
+    
+    
+}
+
+let sample = [Int]()
+for count in 0...5
+{
+    sample.append(count*10)
+}
+for spot in sample
+{
+    print(spot)
+}
